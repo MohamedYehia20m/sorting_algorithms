10f795e0cbcfe736b38e5d7d0285b197384275ca
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-12-17 08:08:59 -0800

    betty correction

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -8,10 +8,10 @@
  **/
 void swap(int *a, int *b)
 {
-    int t = *a;
+	int t = *a;
 
-    *a = *b;
-    *b = t;
+	*a = *b;
+	*b = t;
 }
 
 /**
@@ -25,63 +25,64 @@ void swap(int *a, int *b)
  */
 void maxHeapify(int *array, size_t size, int idx, size_t n)
 {
-    int largest = idx;       /* Initialize largest as root*/
-    int left = 2 * idx + 1;  /* left = (idx << 1) + 1*/
-    int right = 2 * idx + 2; /* right = (idx + 1) << 1*/
+	int largest = idx;       /* Initialize largest as root*/
+	int left = 2 * idx + 1;  /* left = (idx << 1) + 1*/
+	int right = 2 * idx + 2; /* right = (idx + 1) << 1*/
 
-    /* See if left child of root exists and is greater than root*/
-    if (left < (int)n && array[left] > array[largest])
-        largest = left;
+	/* See if left child of root exists and is greater than root*/
+	if (left < (int)n && array[left] > array[largest])
+		largest = left;
 
-    /**
-     * See if right child of root exists and is greater than
-     *the largest so far
-     */
-    if (right < (int)n && array[right] > array[largest])
-        largest = right;
+	/**
+	 * See if right child of root exists and is greater than
+	 * the largest so far
+	 */
+	if (right < (int)n && array[right] > array[largest])
+		largest = right;
 
-    /* Change root, if needed*/
-    if (largest != idx)
-    {
-        swap(&array[idx], &array[largest]);
-        print_array(array, size);
-        maxHeapify(array, size, largest, n);
-    }
+	/* Change root, if needed*/
+	if (largest != idx)
+	{
+		swap(&array[idx], &array[largest]);
+		print_array(array, size);
+		maxHeapify(array, size, largest, n);
+	}
 }
 
 /**
  * heap_sort -  The main function to sort an array of given size
  * @array: array to sort
  * @size: size of the array
- **/
+ */
 void heap_sort(int *array, size_t size)
 {
-    int i;
-    /**
-     * Start from bottommost and rightmost internal mode and heapify all
-     * internal modes in bottom up way
-     */
-    if (array == '\0' || size < 2)
-        return;
+	int i;
 
-    for (i = (size - 2) / 2; i >= 0; --i)
-        maxHeapify(array, size, i, size);
+	/**
+	 * Start from bottommost and rightmost internal mode and heapify all
+	 * internal modes in bottom up way
+	 */
+	if (array == '\0' || size < 2)
+		return;
 
-    /**
-     * Repeat following steps while heap size is greater than 1.
-     * The last element in max heap will be the minimum element
-     */
-    for (i = (size - 1); i > 0; --i)
-    {
-        /**
-         * The largest item in Heap is stored at the root. Replace
-         *it with the last item of the heap followed by reducing the
-         *size of heap by 1.
-         */
-        swap(&array[0], &array[i]);
-        print_array(array, size);
+	for (i = (size - 2) / 2; i >= 0; --i)
+		maxHeapify(array, size, i, size);
 
-        /* Finally, heapify the root of tree.*/
-        maxHeapify(array, size, 0, i);
-    }
+	/**
+	 * Repeat following steps while heap size is greater than 1.
+	 * The last element in max heap will be the minimum element
+	 */
+	for (i = (size - 1); i > 0; --i)
+	{
+		/**
+		 * The largest item in Heap is stored at the root. Replace
+		 * it with the last item of the heap followed by reducing the
+		 * size of heap by 1.
+		 */
+		swap(&array[0], &array[i]);
+		print_array(array, size);
+
+		/* Finally, heapify the root of tree.*/
+		maxHeapify(array, size, 0, i);
+	}
 }
